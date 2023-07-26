0e9ed2da9a44e8e29760a020800d2bfcfc4c7af0
Afreen09 <57040874+Afreen09@users.noreply.github.com>
2023-07-26 19:02:08 +0530

    Update stremlit_app.py

--- stremlit_app.py
+++ stremlit_app.py
@@ -1,7 +1,8 @@
 import streamlit
 streamlit.title('Had Breakfast')
 streamlit.header('Breakfast Menu')
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
-streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
