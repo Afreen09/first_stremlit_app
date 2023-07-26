96411e645e47a8627f06bda019cacca0a0a54b0f
Afreen09 <57040874+Afreen09@users.noreply.github.com>
2023-07-26 18:59:49 +0530

    Update stremlit_app.py

--- stremlit_app.py
+++ stremlit_app.py
@@ -1,5 +1,6 @@
 import streamlit
-streamlit.title('Had Dinner')
-streamlit.header('Dinner Menu')
-streamlit.text('Vegitables, Non-Vegitables')
-streamlit.text('Apple, Bitroot')
+streamlit.title('Had Breakfast')
+streamlit.header('Breakfast Menu')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
